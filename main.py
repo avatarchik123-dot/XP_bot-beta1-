cf8d7388c480ec1627861d71b56dd3cd67fb22ec
avatarchik123-dot <avatarchik123@gmail.com>
2026-03-03 19:50:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from aiogram import Bot, Dispatcher, types
-from aiogram.types import ParseMode
+from aiogram.enums import ParseMode
 from aiogram.utils import executor
 
 from xp_engine import calculate_text_xp, calculate_media_xp, can_gain_xp
