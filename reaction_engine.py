9ad9ef546db56d7a7568b4d6e701b4398d4495d6
avatarchik123-dot <avatarchik123@gmail.com>
2026-03-04 23:32:36 +0200

    Delete reaction_engine.py

--- reaction_engine.py
+++
@@ -1,6 +0,0 @@
-from level_engine import add_xp
-from config import MAX_REACTION_XP
-
-def on_reaction(user_id, group_id, reactions_count):
-    xp = min(reactions_count, MAX_REACTION_XP)
-    add_xp(user_id, group_id, xp)
