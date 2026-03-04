0829e0ae8bf18b99af61d97d7dff0732db18e40e
avatarchik123-dot <avatarchik123@gmail.com>
2026-03-04 23:27:25 +0200

    Create services/file_manager.py

---
+++ services/file_manager.py
@@ -0,0 +1,29 @@
+import json
+import os
+import shutil
+
+DATA_FILES = [
+    "groups.json",
+    "levels.json",
+    "levels_backup.json",
+    "temp_setup.json"
+]
+
+def init_files():
+    os.makedirs("data", exist_ok=True)
+    for file in DATA_FILES:
+        path = f"data/{file}"
+        if not os.path.exists(path):
+            with open(path, "w") as f:
+                json.dump({}, f)
+
+def load(file):
+    with open(f"data/{file}", "r") as f:
+        return json.load(f)
+
+def save(file, data):
+    if file == "levels.json":
+        shutil.copy("data/levels.json", "data/levels_backup.json")
+
+    with open(f"data/{file}", "w") as f:
+        json.dump(data, f, indent=4)
