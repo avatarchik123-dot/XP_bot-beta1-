3bc693d61df8fb7fbc6827df26fa83b4e75041e8
avatarchik123-dot <avatarchik123@gmail.com>
2026-03-05 12:53:34 +0200

    Update __init__.py

--- engines/__init__.py
+++ engines/__init__.py
@@ -1,11 +1,11 @@
+from .xp_engine import router as xp_router
 from .reaction_engine import router as reaction_router
-from .level_engine import router as level_router
 from .admin_engine import router as admin_router
-from .hr_engine import router as hr_router
+from .level_engine import router as level_router
 
 routers = [
+    xp_router,
     reaction_router,
-    level_router,
     admin_router,
-    hr_router
+    level_router
 ]
