a9b661be27bd06ea0842396ab99c1094a8306308
avatarchik123-dot <avatarchik123@gmail.com>
2026-03-05 12:48:55 +0200

    Update __init__.py

--- engines/__init__.py
+++ engines/__init__.py
@@ -1,1 +1,11 @@
-{}
+from .reaction_engine import router as reaction_router
+from .level_engine import router as level_router
+from .admin_engine import router as admin_router
+from .hr_engine import router as hr_router
+
+routers = [
+    reaction_router,
+    level_router,
+    admin_router,
+    hr_router
+]
