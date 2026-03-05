6f13be714c54ee1659a981a4d1b73c506993bfaa
avatarchik123-dot <avatarchik123@gmail.com>
2026-03-05 23:56:13 +0200

    Update level_engine.py

--- engines/level_engine.py
+++ engines/level_engine.py
@@ -22,29 +22,26 @@ async def level_system(message: Message):
     if message.chat.type == "private":
         return
 
-groups = load_json("groups.json")
+groups = load_json("data/groups.json")
 
-chat_id = str(message.chat.id)
-user_id = str(message.from_user.id)
-
-if chat_id not in groups:
+if str(chat_id) not in groups:
     return
 
-if "users" not in groups[chat_id]:
-    groups[chat_id]["users"] = {}
+if "users" not in groups[str(chat_id)]:
+    groups[str(chat_id)]["users"] = {}
 
-user = groups[chat_id]["users"].get(user_id)
+user = groups[str(chat_id)]["users"].get(str(user_id))
 
-    if not user:
-        return
+if not user:
+    groups[str(chat_id)]["users"][str(user_id)] = {
+        "xp": 0,
+        "level": 1
+    }
+    user = groups[str(chat_id)]["users"][str(user_id]]
 
-    new_level = check_level(user["xp"])
+user["xp"] += 5
 
-    if new_level > user["level"]:
-        user["level"] = new_level
-
-        save_json("groups.json", groups)
-
+save_json("data/groups.json", groups)
         await message.answer(
             f"{message.from_user.first_name} достиг уровня {new_level}"
         )
