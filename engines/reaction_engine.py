5d3c6a203c1f2013166a866c8b7c4148a15bc0c6
avatarchik123-dot <avatarchik123@gmail.com>
2026-03-05 17:53:27 +0200

    Update reaction_engine.py

--- engines/reaction_engine.py
+++ engines/reaction_engine.py
@@ -1,19 +1,30 @@
+from aiogram import Router
+from aiogram.types import MessageReactionUpdated
+
 from services.file_manager import load_json, save_json
 
-# Начисление XP за реакцию
-def add_reaction_xp(chat_id: int, message_id: int, user_id: int):
-    levels = load_json("levels")
-    if str(chat_id) not in levels:
-        levels[str(chat_id)] = {}
-    user_data = levels[str(chat_id)].get(str(user_id), {"xp": 0, "level": 0})
+router = Router()
 
-    # Лимит 50 XP на сообщение
-    msg_key = f"{message_id}_reactions"
-    reactions = user_data.get(msg_key, 0)
-    if reactions < 50:
-        user_data["xp"] += 1
-        user_data[msg_key] = reactions + 1
 
-    levels[str(chat_id)][str(user_id)] = user_data
-    save_json("levels", levels)
-    return user_data
+@router.message_reaction()
+async def reaction_xp(event: MessageReactionUpdated):
+
+    chat_id = str(event.chat.id)
+    user_id = str(event.user.id)
+
+    groups = load_json("groups.json")
+
+    if chat_id not in groups:
+        return
+
+    users = groups[chat_id]["users"]
+
+    if user_id not in users:
+        users[user_id] = {
+            "xp": 0,
+            "level": 1
+        }
+
+    users[user_id]["xp"] += 1
+
+    save_json("groups.json", groups)
