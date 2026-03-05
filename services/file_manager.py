ec3f0dfb343e1c6a2837bc9e74fad041d84653c5
avatarchik123-dot <avatarchik123@gmail.com>
2026-03-05 23:21:58 +0200

    Update file_manager.py

--- services/file_manager.py
+++ services/file_manager.py
@@ -1,32 +1,29 @@
 import json
 import os
 
-DATA_PATH = "data"
+DATA_DIR = "data"
 
 
-def _path(name):
-    return os.path.join(DATA_PATH, name)
+def _path(filename: str):
+    return os.path.join(DATA_DIR, filename)
 
 
-def load_json(filename):
+def load_json(filename: str):
     path = _path(filename)
 
     if not os.path.exists(path):
-        with open(path, "w") as f:
+        with open(path, "w", encoding="utf-8") as f:
             json.dump({}, f)
 
-    with open(path, "r") as f:
-        return json.load(f)
+    with open(path, "r", encoding="utf-8") as f:
+        try:
+            return json.load(f)
+        except:
+            return {}
 
 
-def save_json(filename, data):
+def save_json(filename: str, data: dict):
     path = _path(filename)
 
-    with open(path, "w") as f:
+    with open(path, "w", encoding="utf-8") as f:
         json.dump(data, f, indent=4)
-
-
-def update_json(filename, key, value):
-    data = load_json(filename)
-    data[key] = value
-    save_json(filename, data)
