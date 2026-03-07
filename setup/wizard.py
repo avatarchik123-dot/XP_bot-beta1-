5d3c7ca8061d745f145159c51abf2bf12b62a9d7
avatarchik123-dot <avatarchik123@gmail.com>
2026-03-08 00:33:50 +0200

    Update wizard.py

--- setup/wizard.py
+++ setup/wizard.py
@@ -1,1 +1,13 @@
-print("Wizard setup")
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.filters import Command
+
+@router.message(Command("settings"))
+async def open_settings(message: Message):
+
+    kb = InlineKeyboardMarkup(
+        inline_keyboard=[
+            [InlineKeyboardButton(text="⚙️ Настройки", callback_data="settings")]
+        ]
+    )
+
+    await message.answer("Меню настроек", reply_markup=kb)
