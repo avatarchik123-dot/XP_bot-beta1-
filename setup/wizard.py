a8adab3cc6de16ab4d2d474980a53a64d53e5a8e
avatarchik123-dot <avatarchik123@gmail.com>
2026-03-08 00:34:22 +0200

    Update wizard.py

--- setup/wizard.py
+++ setup/wizard.py
@@ -11,3 +11,19 @@ async def open_settings(message: Message):
     )
 
     await message.answer("Меню настроек", reply_markup=kb)
+
+@router.callback_query(F.data == "settings")
+async def settings_menu(call: CallbackQuery):
+
+    kb = InlineKeyboardMarkup(
+        inline_keyboard=[
+            [InlineKeyboardButton(text="📊 Количество уровней", callback_data="set_levels")],
+            [InlineKeyboardButton(text="📏 Дистанция XP", callback_data="set_distance")],
+            [InlineKeyboardButton(text="🏷 Названия уровней", callback_data="set_names")]
+        ]
+    )
+
+    await call.message.edit_text(
+        "Настройки уровней",
+        reply_markup=kb
+    )
