2c96b5dc6e04817d6892a1069996b7e83e132bee
avatarchik123-dot <avatarchik123@gmail.com>
2026-03-07 19:22:51 +0200

    Update reaction.py

--- engines/reaction.py
+++ engines/reaction.py
@@ -19,7 +19,24 @@ async def reaction_handler(event: MessageReactionUpdated):
         return
 
     reactions.insert({
-        "key": key,
-        "message_id": message_id,
-        "reactor": reactor
+        "key": key
     })
+
+    message_author = event.actor_chat
+
+    if not message_author:
+        return
+
+    author_id = message_author.id
+
+    user = users.get((User.user_id == author_id) & (User.chat_id == chat_id))
+
+    if not user:
+        return
+
+    xp_total = user["xp"] + XP_REACTION
+
+    users.update(
+        {"xp": xp_total},
+        (User.user_id == author_id) & (User.chat_id == chat_id)
+    )
