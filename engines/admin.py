6468d6bdb398cf83c92727dd61506c62fa9bb8ef
avatarchik123-dot <avatarchik123@gmail.com>
2026-03-09 01:54:57 +0200

    Update admin.py

--- engines/admin.py
+++ engines/admin.py
@@ -5,6 +5,7 @@ from aiogram.types import Message
 from services.utils import send_temp
 from services.database import groups, Group
 from config import DEFAULT_XP_STEP, DEFAULT_MAX_LEVEL
+from services.database import add_xp, remove_xp
 
 router = Router()
 
@@ -36,3 +37,67 @@ async def init_group(message: Message):
     })
 
     await send_temp(message,"Группа инициализирована")
+
+@router.message(Command("addxp"))
+async def add_xp_cmd(message: Message):
+
+    args = message.text.split()
+
+    if len(args) < 3 or not message.entities:
+        await send_temp(message,"Пример:\n/addxp @user 50")
+        return
+
+    if not args[2].isdigit():
+        await send_temp(message,"XP должно быть числом")
+        return
+
+    xp = int(args[2])
+
+    if xp < 1 or xp > 500:
+        await send_temp(message,"XP должно быть 1-500")
+        return
+
+    user = message.entities[0].user
+
+    new_xp = add_xp(
+        message.chat.id,
+        user.id,
+        xp
+    )
+
+    await send_temp(
+        message,
+        f"Добавлено {xp} XP\nТеперь: {new_xp}"
+    )
+
+@router.message(Command("removexp"))
+async def remove_xp_cmd(message: Message):
+
+    args = message.text.split()
+
+    if len(args) < 3 or not message.entities:
+        await send_temp(message,"Пример:\n/removexp @user 50")
+        return
+
+    if not args[2].isdigit():
+        await send_temp(message,"XP должно быть числом")
+        return
+
+    xp = int(args[2])
+
+    if xp < 1 or xp > 500:
+        await send_temp(message,"XP должно быть 1-500")
+        return
+
+    user = message.entities[0].user
+
+    new_xp = remove_xp(
+        message.chat.id,
+        user.id,
+        xp
+    )
+
+    await send_temp(
+        message,
+        f"Снято {xp} XP\nТеперь: {new_xp}"
+    )
