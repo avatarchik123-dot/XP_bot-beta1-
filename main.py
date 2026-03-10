69b9deb086c15e2556ed8b92bb93a00428d8755b
avatarchik123-dot <avatarchik123@gmail.com>
2026-03-10 23:08:20 +0200

    Update main.py

--- main.py
+++ main.py
@@ -18,8 +18,8 @@ async def main():
 
     dp.include_router(wizard_router)
     dp.include_router(admin_router)
-    dp.include_router(level_router)
     dp.include_router(pic_router)
+    dp.include_router(level_router)
     dp.include_router(reaction_router)
     
 
