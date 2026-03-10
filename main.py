c61413318789f1c6bdb18e2bd6b102ca82f0f829
avatarchik123-dot <avatarchik123@gmail.com>
2026-03-10 23:29:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ from engines.admin import router as admin_router
 from engines.level import router as level_router
 from engines.reaction import router as reaction_router
 from setup.wizard import router as wizard_router
-from services.pic import router as pic_router
 
 
 async def main():
@@ -18,7 +17,6 @@ async def main():
 
     dp.include_router(wizard_router)
     dp.include_router(admin_router)
-    dp.include_router(pic_router)
     dp.include_router(level_router)
     dp.include_router(reaction_router)
     
